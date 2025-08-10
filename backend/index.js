8ddee0640f787bbe1061b661ba383b61a772ffca
Nelcy17 <nelcy.rathore07@gmail.com>
2025-08-10 19:13:13 +0530

    Resolve merge conflict in index.js

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,6 @@ import courseRoute from "./routes/course.route.js";
 import userRoute from "./routes/user.route.js";
 import adminRoute from "./routes/admin.route.js";
 import paymentRoute from "./routes/payment.route.js";
-<<<<<<< HEAD
-=======
-
->>>>>>> e1b21ea (first commit)
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -26,31 +22,21 @@ app.use(cors({
 })
 );
 
-//middleware   
-app.use(express.json()); //data ko parse krna json s
+
+app.use(express.json()); 
 app.use(cookieParser());
 
-app.use(express.urlencoded({extended:true})); //data ko parse krna urlencoded s
+app.use(express.urlencoded({extended:true}));  
 
-//file upload 
+
 app.use(fileUpload({
     useTempFiles : true,
     tempFileDir : '/tmp/'
 })
 );
 
-<<<<<<< HEAD
 
 
-=======
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT" , "DELETE"],
-    allowedHeaders: ["Content-Type","Authorization"],
-})
-);
->>>>>>> e1b21ea (first commit)
 
 const port = process.env.PORT || 3000;
 const DB_URI=process.env.MONGO_URI;
@@ -62,14 +48,14 @@ try{
     console.log(error);
 }
 
-//defining routes
+
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRoute);
 app.use("/api/v1/payment", paymentRoute);
-app.use("/api/v1", paymnt);//new
+app.use("/api/v1", paymnt);
 
-//cloudinary configuration
+
 cloudinary.config({ 
     cloud_name: process.env.cloud_name, 
     api_key: process.env.api_key, 
@@ -82,8 +68,6 @@ app.listen(port, () => {
 
 
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> e1b21ea (first commit)
+
